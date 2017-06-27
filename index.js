3a20987daf4dacc85619e8a5bfbe76af9d96e5d3
Bowen Feng <bowenfeng.dev@gmail.com>
2017-06-27 01:00:35 -0400

    support top level menu.

--- index.js
+++ index.js
@@ -4,45 +4,82 @@ var Alexa = require("alexa-sdk");
 var logHelper = require('./logHelper');
 
 var states = {
-    WATCHED_MODE: '_WATCHED_MODE',
-
+    ADD_MODE: "_ADD_MODE",
+    REVIEW_MODE: "_REVIEW_MODE"
 }
 
 var handlers = {
     "NewSession": function() {
         logHelper.logSessionStarted(this.event.session);
-        this.emit(":ask", "What movie have you watched?")
+        this.emit(":ask", "Do you want to add a new movie, or review your movie history?");
     },
 
-    "AddWatchedMovieIntent": function() {
+    "MainMenuIntent": function() {
         console.log(this.event.request);
         var filledSlots = delegateSlotCollection.call(this);
-        var movie = this.event.request.intent.slots.movie.value;
-        var date = this.event.request.intent.slots.date.value;
-        var message = "OK, you have watched " + movie + " on " + date;
-        var logMsg = {
-            "eventType": this.event.request.eventType,
-            "movie": movie,
-            "date": date
+        console.log(this.event.request.intent.slots.choice.value);
+        var choice = this.event.request.intent.slots.choice.value;
+
+        if (choice === "review") {
+            console.log("Entering review mode ...")
+            this.handler.state = states.REVIEW_MODE;
+            this.emit('LaunchReview');
+        } else {
+            console.log("Entering add mode ...")
+            this.handler.state = states.ADD_MODE;
+            this.emit('LaunchAdd');
         }
-        console.log("%j", logMsg);
-        this.emit(":tell", message);
     },
+
+    'LaunchAdd': function() {
+        console.log('entered LaunchAdd intent handler...')
+        this.emit(':ask', 'Tell me about the movie you watched.');
+    },
+
     'EndSession' : function (message) {
         console.log("Session Ended with message:" + message);
         this.emit(':saveState', true);
     },
+
     "Unhandled": function () {
         console.log("unhandled");
         var speechOutput = "Please start over again";
         this.emit(":ask", speechOutput, speechOutput);
     }
-}
+};
+
+var stateHandlers = {
+    addModeIntentHandlers : Alexa.CreateStateHandler(states.ADD_MODE, {
+
+        'AddWatchedMovieIntent': function() {
+            console.log(this.event.request);
+            var filledSlots = delegateSlotCollection.call(this);
+            var movie = this.event.request.intent.slots.movie.value;
+            var date = this.event.request.intent.slots.date.value;
+            var message = "OK, you have watched " + movie + " on " + date;
+            var logMsg = {
+                "eventType": this.event.request.eventType,
+                "movie": movie,
+                "date": date
+            }
+            console.log("%j", logMsg);
+            this.emit(":tell", message);
+        },
+    }),
+
+    reviewModeIntentHandlers : Alexa.CreateStateHandler(states.REVIEW_MODE, {
+
+    })
+};
+
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
     alexa.appId = "amzn1.ask.skill.4d4ca562-4f28-41b1-8547-d88dd2b26716";
-    alexa.registerHandlers(handlers);
+    alexa.registerHandlers(
+        handlers,
+        stateHandlers.addModeIntentHandlers,
+        stateHandlers.reviewModeIntentHandlers);
     alexa.execute();
 };
 
